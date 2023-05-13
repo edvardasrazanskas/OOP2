b89db921f0ba737593db1a8472dfd901d1fe5725
ProgramFriend <edvardasras@gmail.com>
2023-05-13 13:51:52 +0300

    -

---
+++ tests.cpp
@@ -0,0 +1,28 @@
+#include "studentas.h"
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+TEST_CASE("Teisingai ivestas vardas")
+{
+    Studentas s("Antanas", "Pavardenis", 7, 8);
+    REQUIRE(s.vardas() == "Antanas");
+}
+
+TEST_CASE("Teisingai ivesta pavarde")
+{
+    Studentas s("Antanas", "Pavardenis", 7, 8);
+    REQUIRE(s.pavarde() == "Pavardenis");
+}
+
+TEST_CASE("Teisingai ivestas vardas")
+{
+    Studentas s("Antanas", "Pavardenis", 7, 8);
+    REQUIRE(s.vidurkis() == 7);
+}
+
+TEST_CASE("Teisingai ivestas vardas")
+{
+    Studentas s("Antanas", "Pavardenis", 7, 8);
+    REQUIRE(s.mediana() == 8);
+}
