59594d66182905cc9a311295a3f8799fbc07e58e
ProgramFriend <edvardasras@gmail.com>
2023-05-14 13:56:25 +0300

    minor bug fixes

--- tests.cpp
+++ tests.cpp
@@ -15,13 +15,13 @@ TEST_CASE("Teisingai ivesta pavarde")
     REQUIRE(s.pavarde() == "Pavardenis");
 }
 
-TEST_CASE("Teisingai ivestas vardas")
+TEST_CASE("Teisingai ivestas vidurkis")
 {
     Studentas s("Antanas", "Pavardenis", 7, 8);
     REQUIRE(s.vidurkis() == 7);
 }
 
-TEST_CASE("Teisingai ivestas vardas")
+TEST_CASE("Teisingai ivesta mediana")
 {
     Studentas s("Antanas", "Pavardenis", 7, 8);
     REQUIRE(s.mediana() == 8);
